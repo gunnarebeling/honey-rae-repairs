408a3a7d8c77fbda2c31966f10c7fecd76c85b96
gunnar ebeling <gunnarebeling@Mac-mini.local>
2024-08-31 16:30:14 -0500

    writing first component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
+import { useState } from "react"
+
 export const App = () => {
-  return <div className="welcome">Welcome to your first React Application!</div>
+const [count, setCount] = useState(0)
+
+const handleBtnClick = () => {
+  setCount(count + 1)
+  console.log("clicked");
+  
 }
+return <>
+          <h1>Hello</h1>
+          <div>this is amazing!</div>
+          <button className="btn-secondary"onClick={handleBtnClick}>
+            click me
+          </button>
+          <div>count: {count}</div>
+        </>}
+  
