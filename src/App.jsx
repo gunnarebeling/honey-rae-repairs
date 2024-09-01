749028a3531c4431b342e2397fc30219fbc3e914
gunnar ebeling <gunnarebeling@Mac-mini.attlocal.net>
2024-09-01 10:21:52 -0500

    making tickets

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { getAllTickets } from "./services/ticketServices"
 
 export const App = () => {
-const [count, setCount] = useState(0)
+const [allTickets, setAllTickets] = useState([])
 
-const handleBtnClick = () => {
-  setCount(count + 1)
-  console.log("clicked");
-  
-}
-return <>
-          <h1>Hello</h1>
-          <div>this is amazing!</div>
-          <button className="btn-secondary"onClick={handleBtnClick}>
-            click me
-          </button>
-          <div>count: {count}</div>
-        </>}
+useEffect(() => {
+  getAllTickets().then((ticketArray) => {
+    setAllTickets(ticketArray)
+  })
+}, [])
+
+return (
+<div className="tickets-container">
+  <h2>tickets</h2>
+  <article className="tickets">
+    {allTickets.map(ticket =>{
+      return (
+        <section className="ticket">
+          <header className="ticket-info">#{ticket.id}</header>
+        </section>
+      )
+    })}
+  </article>
+
+</div>
+
+)
+          
+        }
   
