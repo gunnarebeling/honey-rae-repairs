9a7497d131db39635fbe71aedbebe3474f866a7e
gunnar ebeling <gunnarebeling@Mac-mini.local>
2024-09-03 15:57:51 -0500

    finished first column

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,54 @@
 import { useEffect, useState } from "react"
 import { getAllTickets } from "./services/ticketServices"
+import "./App.css"
+
 
 export const App = () => {
 const [allTickets, setAllTickets] = useState([])
+const [showEmergencyOnly, setShowEmergency] = useState(false)
+const [filterdTickets, setFilteredTickets] = useState([])
+
 
 useEffect(() => {
   getAllTickets().then((ticketArray) => {
     setAllTickets(ticketArray)
+    
   })
+  
 }, [])
+
+useEffect(() =>{
+  if(showEmergencyOnly){
+    const emergencyTickets = allTickets.filter( ticket => ticket.emergency)
+    setFilteredTickets(emergencyTickets)
+    
+    
+  }else{
+    setFilteredTickets(allTickets)
+  }
+
+}, [showEmergencyOnly, allTickets])
 
 return (
 <div className="tickets-container">
   <h2>tickets</h2>
+  <div>
+    <button className="filter-btn btn-primary" onClick={()=>setShowEmergency(true)}>Emergency</button>
+    <button className="filter-btn btn-info" onClick={() => setShowEmergency(false)}>Show All</button>
+  </div>
+
   <article className="tickets">
-    {allTickets.map(ticket =>{
+    {filterdTickets.map(ticket =>{
       return (
-        <section className="ticket">
+        <section className="ticket" key={ticket.id}>
           <header className="ticket-info">#{ticket.id}</header>
+          <div>{ticket.description}</div>
+          <footer>
+              <div>
+                <div className="ticket-info">emergency</div>
+                <div>{ticket.emergency? "yes" : "no"}</div>
+              </div>
+          </footer>
         </section>
       )
     })}
