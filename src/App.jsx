70dab58ec43ea801d44b678063e13960e0a63da5
Gunnar Ebeling <gunnarebeling@icloud.com>
2024-09-07 13:05:22 -0500

    added NavBar and outlet

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,26 @@
 import "./App.css"
 import { CustomerList } from "./components/customers/CustomerList"
 import { EmployeesList } from "./components/employees/EmployeesList"
+import { NavBar } from "./components/nav/NavBar"
 import { TicketList } from "./components/tickets/TicketList"
+import { Routes, Route, Outlet } from "react-router-dom"
 
 
 export const App = () => {
-  return <>
-    {/* <TicketList/> */}
-    {/* <CustomerList/> */}
-    <EmployeesList/>
-  </>
-          
+  return( 
+  <Routes>
+    <Route 
+      path="/" 
+      element={
+        <>
+          <NavBar/>
+          <Outlet />
+        </>
+      }
+    >
+      <Route path="tickets" element={<TicketList/>} />
+    </Route>
+  </Routes>
+  )
 }
   
